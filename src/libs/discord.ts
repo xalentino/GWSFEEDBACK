14a8145d5d4c3659a00065037da83e964db00d23
xalentino <rookielover16@gmail.com>
2026-07-23 13:12:51 -0400

    Restore Discord role sync route and helper

--- src/libs/discord.ts
+++ src/libs/discord.ts
@@ -1,6 +1,6 @@
 const ROLE_MAP: Record<string, string> = {
-  '1454594433965359165': "owner", // Lead Developer
-  '1500426556160348360': "admin", // Manging Director
+  '1494144464715907133': "owner", // Group Holder
+  '1363545887565021364': "admin", // Executive Team
   '1501295951405645926': "developer", // engineering team
   '1500426637744013422': "staff", // Team Boostify role
   '1500439003672088588': "contributor" // contributor, as you guessed
