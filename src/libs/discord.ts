c8673620f123c4f51781a36c29bbc69ff54e6934
xalentino <rookielover16@gmail.com>
2026-07-23 14:30:15 -0400

    Fix role mapping for moderation permissions

--- src/libs/discord.ts
+++ src/libs/discord.ts
@@ -2,6 +2,7 @@ const ROLE_MAP: Record<string, string> = {
   '1494144464715907133': "owner", // Group Holder
   '1363545887565021364': "admin", // Executive Team
   '1471333401431638038': "developer", // Development Team
+  '1356769748611895459': "staff", // Corporate Team
   '1357075397409243266': "staff", // Management Team
   '1356766404103246015': "staff" // staff
 };
